a7ade9a64a48f1d90f3d5d023d88e658da2a356e
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-10-06 22:57:43 +0300

    Add CreateMoneyTransfersTable migration.

---
+++ src/database/migrations/2019_08_13_125200_create_money_transfers_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Contract\Entity\MoneyTransfer\Field\NameInterface as FieldNameInterface;
+use App\Contract\Entity\MoneyTransfer\TableInterface;
+use App\Contract\Entity\MoneyChange\Field\NameInterface as MoneyChangeFieldNameInterface;
+use App\Contract\Entity\MoneyChange\TableInterface as MoneyChangeTableInterface;
+use App\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoneyTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(TableInterface::NAME, function (Blueprint $table) {
+            $table->bigIncrements(FieldNameInterface::ID);
+
+            $this->addRelationColumn(
+                $table,
+                FieldNameInterface::INCOME_MONEY_CHANGE_ID,
+                MoneyChangeTableInterface::NAME,
+                MoneyChangeFieldNameInterface::ID
+            );
+
+            $this->addRelationColumn(
+                $table,
+                FieldNameInterface::OUTCOME_MONEY_CHANGE_ID,
+                MoneyChangeTableInterface::NAME,
+                MoneyChangeFieldNameInterface::ID
+            );
+
+            $table->text(FieldNameInterface::NOTE)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(TableInterface::NAME);
+    }
+}
