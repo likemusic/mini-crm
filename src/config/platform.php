63876e27c68fe7a22164a39a9c0f3d56cf155bcf
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-10-22 15:56:10 +0300

    New Order form js fixes.

--- src/config/platform.php
+++ src/config/platform.php
@@ -80,7 +80,9 @@ return [
     */
 
     'resource' => [
-        'stylesheets' => [],
+        'stylesheets' => [
+            '/css/app.css',
+        ],
         'scripts'     => [],
     ],
 
--- src/resources/js/order.js
+++ src/resources/js/order.js
@@ -5,17 +5,43 @@
 
 
 let data = {
-    count: 5,
-    price: 10,
+    count: 1,
+    price: 0,
+
+    product: {
+        approximate_price: 0,
+        salling_price: 0,
+        warehouse_total_quantity: 0
+    }
 };
+
+const productFieldName = 'product[product_id]';
+const approximatePriceFieldName = 'product[approximate_price]';
+const sellingPriceFieldName = 'product[selling_price]';
+const warehouseTotalQuantityFieldName = 'product[warehouse_total_quantity]';
 
 const amountFieldName = 'product[amount]';
 const countFieldName = 'product[count]';
 const priceFieldName = 'product[item_price]';
 
+bindToField(approximatePriceFieldName, 'product.approximate_price', data);
+bindToField(sellingPriceFieldName, 'product.salling_price', data);
+bindToField(warehouseTotalQuantityFieldName, 'product.warehouse_total_quantity', data);
+
 bindToField(amountFieldName, 'amount', data);
 bindToField(countFieldName, 'count', data);
 bindToField(priceFieldName, 'price', data);
+
+$(function () {
+    $(getFieldByName(productFieldName)).on('select2:select', function (event) {
+        var selectedData = event.params.data;
+
+        data.product.approximate_price = selectedData.approximate_price;
+        data.product.salling_price = selectedData.selling_price;
+        data.price = selectedData.selling_price;
+        data.product.warehouse_total_quantity = selectedData.warehouse_total_quantity;
+    });
+});
 
 function bindToField(fieldName, dataKey, data) {
     let field = getFieldByName(fieldName);
@@ -36,35 +62,13 @@ function bindToField(fieldName, dataKey, data) {
         });
     }
 
-    function getFieldByName(fieldName) {
-        return document.getElementsByName(fieldName)[0];
-    }
-
-    function createFieldTemplate(field, datKey) {
+    function createFieldTemplate(field, dataKey) {
         field.setAttribute('v-model', dataKey);
 
         return field.outerHTML;
     }
 }
 
-//
-// const Test = {
-//     // data: data,
-//     template: amountTemplate,
-//     props: ['count', 'price'],
-//     computed: {
-//         amount: function () {
-//             return this.$data.count * this.$data.price;
-//         },
-//     }
-// };
-//
-// const TestCtor = Vue.extend(Test);
-// const vm = new TestCtor({
-//     // propsData: data,
-//     data: data
-// }).$mount(amountField);
-//
-// const app = new Vue({
-//     el: '#app',
-// });
+function getFieldByName(fieldName) {
+    return document.getElementsByName(fieldName)[0];
+}
