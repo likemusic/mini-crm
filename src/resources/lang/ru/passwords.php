5141425d6a1bc0cc8c760de2e957dcb18c7f3490
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-10-26 01:18:26 +0300

    Translation fixes.

---
+++ src/resources/lang/ru/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароль должен состоять не менее чем из 8-ми символов и совспадать с полем подстверждения.',
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Мы отправиливам e-mail со ссылкой для сброса пароля!',
+    'token' => 'Неверный токен сброса пароля.',
+    'user' => "Нет пользователя с указанным e-mail адресом.",
+];
