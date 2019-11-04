d2229c09a294b32f21a1b81c43a9ee6500530751
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-11-04 21:45:01 +0300

    Fix users table migration.

--- src/app/Contract/Entity/User/Field/NameInterface.php
+++ src/app/Contract/Entity/User/Field/NameInterface.php
@@ -6,6 +6,7 @@ use App\Contract\Entity\Base\Field\Name\EntityInterface;
 
 interface NameInterface extends EntityInterface
 {
+    const NAME = 'name';
     const EMAIL = 'email';
     const EMAIL_VERIFIED_AT = 'email_verified_at';
     const PASSWORD = 'password';
